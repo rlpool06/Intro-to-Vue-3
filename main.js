eba75c4249670fa9dba97630ab9baa8086bafd63
Ryan Pool <ryanpool@ryans-mbp.lan>
2022-01-13 14:07:21 -0600

    Final solutions through video 11

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ],
+            cart: [],
+            premium: false,
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
         },
-        removeFromCart() {
-            this.cart -= 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
+        removeFromCart(id) {
+            this.cart.splice(this.cart.indexOf(id),
+                1)
         },
     },
 })
